882e025521c293b98d6c22874bb1a2984907d0a9
Michael <michaeljbeam@gmail.com>
2016-06-09 16:56:45 -0500

    added option to import v1 wallet

--- factom-walletd.go
+++ factom-walletd.go
@@ -20,8 +20,7 @@ import (
 var homedir = func() string {
 	usr, err := user.Current()
 	if err != nil {
-		log.Println(err)
-		os.Exit(1)
+		log.Fatal(err)
 	}
 	return usr.HomeDir
 }()
@@ -32,11 +31,23 @@ func main() {
 		pflag = flag.Int("p", 8089, "set the port to host the wsapi")
 		wflag = flag.String("w", fmt.Sprint(homedir, "/.factom/wallet"),
 			"set the default wallet location")
+		iflag = flag.String("i", fmt.Sprint(homedir, ""),
+			"import a version 1 wallet")
 	)
 	flag.Parse()
 
 	port := *pflag
 
+	if *iflag != "" {
+		log.Printf("Importing version 1 wallet %s into %s", *iflag, *wflag)
+		w, err := wallet.ImportV1Wallet(*iflag, *wflag)
+		if err != nil {
+			log.Fatal(err)
+		}
+		w.Close()
+		os.Exit(0)
+	}
+	
 	// open or create a new wallet file
 	fctWallet, err := wallet.NewOrOpenWallet(*wflag)
 	if err != nil {
