2bc28ecae580ac436ef9768311475038b5ab23d9
Brian Deery <brian@factom.com>
2017-08-25 11:43:31 -0500

    tweak for building with vendor dir

--- build.sh
+++ build.sh
@@ -2,7 +2,8 @@ vendorver=./vendor/github.com/FactomProject/factom/wallet/VERSION
 godirver=$GOPATH/src/github.com/FactomProject/factom/wallet/VERSION
 
 if [ -f $vendorver ]; then
-    go install -ldflags "-X github.com/FactomProject/factom/wallet.WalletVersion=`cat $vendorver`" -v
+    go install -ldflags "-X github.com/FactomProject/factom-walletd/vendor/github.com/FactomProject/factom/wallet.WalletVersion=`cat $vendorver`" -v
 else
     go install -ldflags "-X github.com/FactomProject/factom/wallet.WalletVersion=`cat $godirver`" -v
 fi
+
